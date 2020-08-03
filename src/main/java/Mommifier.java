6c0d58967c2c2ecb4b8e839f9aa462a18fff73c3
WenqiangLiao <18840965351@163.com>
2020-08-03 18:16:19 +0800

    [Wenqiang Liao] feat: should count the percent of vowels in string

---
+++ src/main/java/Mommifier.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Mommifier {
+    final ArrayList<String> VOWELS = new ArrayList<>(Arrays.asList("a", "e", "i", "o", "u"));
+
+    public int counterPercentOfVowels(String string) {
+        int length = string.length();
+        int vowelNumber = 0;
+
+        ArrayList<String> arrayString = new ArrayList<>(Arrays.asList(string.split("")));
+
+        for (String str:arrayString) {
+            if (VOWELS.contains(str)) {
+                vowelNumber += 1;
+            }
+        }
+
+        int percent = vowelNumber * 100 / length;
+        return  percent;
+    }
+}
--- src/test/java/MommifierTest.java
+++ src/test/java/MommifierTest.java
@@ -1,2 +1,14 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MommifierTest {
+    @Test
+    void should_count_the_percent_of_vowels_in_string () {
+        Mommifier mommifier = new Mommifier();
+        String str = "abcdef";
+
+        int result = mommifier.counterPercentOfVowels(str);
+        
+        assertEquals(33, result);
+    }
 }
